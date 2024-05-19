75a15b62f654fca74c4aaf7cdb6b2714db22b2c8
Andrii Halai <halaiandriy@gmail.com>
2024-05-19 14:08:05 +0300

    Add title for apartments list

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,9 +6,28 @@ function App() {
   return (
     <div className="App">
       <h1>Apartments Marketplace</h1>
-      <div>
+      <div className="container">
         <h2>&#129297; Create a new rent</h2>
         <ApartmentForm />
+      </div>
+      <div className="apartments-list-container">
+        <h2>&#127969;Available Apartments (number)</h2>
+        <form className="sorting-form">
+          <label htmlFor="rooms">Rooms:</label>
+          <input type="number" className="apartment-sort-option" />
+          <label htmlFor="price">Sort by price:</label>
+          <select
+            name="price"
+            className="apartment-sort-option"
+            id="price-sort-dropdown"
+          >
+            <option value="asc">Lowest first</option>
+            <option value="desc">Highest first</option>
+            <option value="" selected>
+              None
+            </option>
+          </select>
+        </form>
       </div>
     </div>
   );
