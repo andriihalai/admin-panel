05a7ba519a55e7b46f0889008b6bae63421cfa1c
Andrii Halai <halaiandriy@gmail.com>
2024-05-20 09:17:48 +0300

    Extract component instead of adding it to the root

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,11 @@
 import React, { useState } from "react";
 import "./App.css";
-import ApartmentForm from "./components/ApartmentForm";
-import AvailableApartments from "./components/AvailableApartments";
-import axios from "axios";
+import ApartmentManager from "./components/ApartmentManager";
 
 function App() {
-  const [apartments, setApartments] = useState([]);
-
-  const fetchApartments = () => {
-    axios
-      .get("http://localhost:8000/apartments")
-      .then((response) => setApartments(response.data))
-      .catch((error) => console.error("Error fetching apartments:", error));
-  };
-
   return (
     <div className="App">
-      <h1>Apartments Marketplace</h1>
-      <div className="container">
-        <h2>&#129297; Create a new rent</h2>
-        <ApartmentForm fetchApartments={fetchApartments} />
-      </div>
-      <AvailableApartments
-        apartments={apartments}
-        setApartments={setApartments}
-        fetchApartments={fetchApartments}
-      />
+      <ApartmentManager />
     </div>
   );
 }
---
+++ frontend/src/components/ApartmentManager.tsx
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { useState } from "react";
+import ApartmentForm from "./ApartmentForm";
+import AvailableApartments from "./AvailableApartments";
+
+export default function ApartmentManager() {
+  const [apartments, setApartments] = useState([]);
+
+  const fetchApartments = () => {
+    axios
+      .get("http://localhost:8000/apartments")
+      .then((response) => setApartments(response.data))
+      .catch((error) => console.error("Error fetching apartments:", error));
+  };
+
+  return (
+    <>
+      <h1>Apartments Marketplace</h1>
+      <div className="container">
+        <h2>&#129297; Create a new rent</h2>
+        <ApartmentForm fetchApartments={fetchApartments} />
+      </div>
+      <AvailableApartments
+        apartments={apartments}
+        setApartments={setApartments}
+        fetchApartments={fetchApartments}
+      />
+    </>
+  );
+}
